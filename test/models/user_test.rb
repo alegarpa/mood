8b47fec4e0a0f5013ab1fe4c4856de7e4de7f507
Alejandro <alegarpa@berkeley.edu>
2016-02-09 19:56:04 -0800

    Added User Model with basic passwords

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,12 +6,64 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-  	@user = User.new(name: "Billy Joe", email: "bill@ex.com")
+  	@user = User.new(name: "Billy Joe", email: "bill@ex.com", password: "whybbywhy", password_confirmation: "whybbywhy")
+	@user2 = User.new(name: "Troll Yo", email: "troll@KEWL..com", password: "trollol", password_confirmation: "trollol")  
   end
 
   test "should be valid" do
   	assert @user.valid?
   end
+
+  test "name should not be present" do
+  	@user.name = "      "
+  	assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+   test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email addresses should have the right format" do
+  	assert_not @user2.valid?
+  end
+
+   test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+   test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
 
 
